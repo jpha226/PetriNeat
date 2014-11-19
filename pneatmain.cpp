44adbac153630a9254a7ba4889d948d0bae10b35
Josiah <josiah.hanna@utexas.edu>
2014-11-19 13:02:15 -0600

    initial project version

---
+++ pneatmain.cpp
@@ -0,0 +1,68 @@
+/*
+ Copyright 2001 The University of Texas at Austin
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+//#include <stdlib.h>
+//#include <stdio.h>
+//#include <iostream>
+//#include <iomanip>
+//#include <sstream>
+//#include <list>
+//#include <vector>
+//#include <algorithm>
+//#include <cmath>
+//#include <iostream.h>
+//#include "networks.h"
+//#include "neat.h"
+//#include "genetics.h"
+//#include "experiments.h"
+//#include "neatmain.h"
+#include <iostream>
+#include <vector>
+#include "neat.h"
+#include "population.h"
+#include "experiments.h"
+using namespace std;
+
+
+
+int main(int argc, char *argv[]) {
+
+  NEAT::Population *p=0;
+
+  //***********RANDOM SETUP***************//
+  /* Seed the random-number generator with current time so that
+      the numbers will be different every time we run.    */
+  srand( (unsigned)time( NULL ) );
+
+  if (argc != 2) {
+    cerr << "A NEAT parameters file (.ne file) is required to run the experiments!" << endl;
+    return -1;
+  }
+
+  //Load in the params
+  NEAT::load_neat_params(argv[1],true);
+
+  cout<<"loaded"<<endl;
+  
+  cout<<"Beginning experiement: "<<endl;
+  p = petrinet_test(100);
+
+  if (p)
+    delete p;
+
+  return(0);
+ 
+}
+
